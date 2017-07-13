8e8fafa5d59c94d334a86d69185d06e6eec8e038
Chongyang Ma <machy85@gmail.com>
2017-07-13 09:36:13 -0700

    fix linear_svm.py and update

--- assignments/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/assignment1/cs231n/classifiers/linear_svm.py
@@ -44,7 +44,7 @@ def svm_loss_naive(W, X, y, reg):
   dW /= num_train
 
   # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+  loss += 0.5 * reg * np.sum(W * W)
   dW += reg * W
 
   #############################################################################
